e98097377d68eef8997c6db8a0d41856fdf005cf
kztn0112 <ngwn1995@gmail.com>
2021-06-13 17:05:04 +0000

    [Finish]App2-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6c967f07264fe2e06cb87af23b2b0a08d55fc8a467b91f27453e984a7d2bf37760638a7440208f56fe5b9e082dbf8361fb38fe93ba3685ea86545a1b0fa0bbaf'
